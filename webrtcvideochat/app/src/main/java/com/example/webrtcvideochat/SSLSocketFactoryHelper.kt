bdea672a0b84bb6553ad64ab218d2b6092163ba1
ashloverscn <ashloverscn@gmail.com>
2026-02-12 12:16:12 +0530

    Delete webrtcvideochat/app/src/main/java/com/example/webrtcvideochat/SSLSocketFactoryHelper.kt

--- webrtcvideochat/app/src/main/java/com/example/webrtcvideochat/SSLSocketFactoryHelper.kt
+++
@@ -1,7 +0,0 @@
-package com.example.webrtcvideochat
-
-import javax.net.ssl.SSLSocketFactory
-
-object SSLSocketFactoryHelper {
-    fun get(): SSLSocketFactory = SSLSocketFactory.getDefault() as SSLSocketFactory
-}
